041a4fe729e3223bff88af61ebf9400d92e84bfa
Rian Radeck <rianradeck3@gmail.com>
2024-11-06 13:41:23 +0100

    s6 - question 1

--- s6/code.py
+++ s6/code.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 # Paramètres donnés
 T = 0.01
@@ -8,7 +10,7 @@ sigma_w = 0.2
 V = 3.0  # m/s
 omega = 2 * np.pi / 3  # rad/s
 x_b, y_b = 2, 5  # Coordonnées du point fixe
-x0 = np.array([[0, 0, 0]]).T  # État initial estimé
+x0 = np.array([[0, 0, 0]]).T  # État initial Estimated
 P0 = 10 * np.eye(3)  # Covariance initiale
 
 # Matrice de covariance du bruit de processus (pour u = [ΔV, Δω]^T)
@@ -73,59 +75,33 @@ def step(z):
 
     correction(z)
 
-# Filtre de Kalman étendu
-def extended_kalman_filter(z_meas, x0, P0):
-    # Initialisation
-    x_hat = x0
-    P_k = P0
-    estimates = []
-    covariances = []
-
-    for z_k in z_meas:
-        # Prédiction de l'état
-        phi_hat = x_hat[2][0]
-        # x_hat_pred = compute_Phi_k(V, T, phi_hat) @ x_hat
-        x_hat_pred = np.array([
-            [x_hat[0][0] + V * T * np.cos(phi_hat)],
-            [x_hat[1][0] + V * T * np.sin(phi_hat)],
-            [x_hat[2][0] + T * omega]
-        ])
-        # x_hat_pred => x_hat_k+1_sachant_k (3, 1)
-
-
-        # Prédiction de la covariance (3, 3)
-        Phi_k = compute_Phi_k(V, T, phi_hat)
-        Gamma = compute_Gamma(T, phi_hat)
-        P_k_pred = Phi_k @ P_k @ Phi_k.T + Gamma @ Cov_u @ Gamma.T
-        
-        # Calcul de H_k (1, 3)
-        H_k = compute_H_k(x_hat_pred[0][0], x_hat_pred[1][0])
-        
-        # Gain de Kalman (3, 1)
-        S_k = (H_k @ P_k_pred @ H_k.T) + W_k
-        K_k = P_k_pred @ H_k.T @ np.linalg.inv(S_k)
-        
-        # Mise à jour de l'estimation de l'état
-        x_hat = x_hat_pred + K_k @ (z_k - H_k @ x_hat_pred)
-        
-        # Mise à jour de la covariance
-        P_k = (np.eye(3) - K_k @ H_k) @ P_k_pred
-        
-        # Sauvegarde de l'estimation
-        estimates.append(x_hat)
-        covariances.append(P_k)
-    
-    return np.array(estimates), np.array(covariances)
-
 z_meas = np.loadtxt('./Kalmannonlin.txt')
 for z in z_meas:
     step(z)
 
-import matplotlib.pyplot as plt
 
-data = np.array(X_hat).reshape(-1, 3)
+data = np.array(X_hat)[1:].reshape(-1, 3)
 covariances = np.array(P).reshape(-1, 3, 3) 
 vrai = np.loadtxt('./etatvrai.txt')
+
+def plot_error():
+    X = data[:, 0]
+    X_vrai = vrai[0]
+    Y = data[:, 1]
+    Y_vrai = vrai[1]
+    omega = data[:, 2]
+    omega_vrai = vrai[2]
+
+    _X_hat = np.array([X, Y, omega])
+    _X = np.array([X_vrai, Y_vrai, omega_vrai])
+    error = np.linalg.norm(_X - _X_hat, axis=0)
+    
+    fig, ax = plt.subplots(1, 1, figsize=(8, 6))
+    ax.plot(error)
+    ax.set_title("Evolution of the Quadratic Estimation Error")
+    ax.set_xlabel("Iteration")
+    ax.set_ylabel("Quadratic Estimation Error")
+    ax.grid(alpha=0.3)
 
 def plot_estimates():
     X = data[:, 0]
@@ -137,18 +113,18 @@ def plot_estimates():
 
     fig, axs = plt.subplots(3, 1, figsize=(10, 6))
 
-    axs[0].plot(X, label='Estimé')
-    axs[0].plot(X_vrai, label='Vrai')
+    axs[0].plot(X, label='Estimated')
+    axs[0].plot(X_vrai, label='True Value')
     axs[0].set_ylabel('X')
     axs[0].set_title('X over Measures')
 
-    axs[1].plot(Y, label='Estimé')
-    axs[1].plot(Y_vrai, label='Vrai')
+    axs[1].plot(Y, label='Estimated')
+    axs[1].plot(Y_vrai, label='True Value')
     axs[1].set_ylabel('Y')
     axs[1].set_title('Y over Measures')
 
-    axs[2].plot(omega, label='Estimé')
-    axs[2].plot(omega_vrai, label='Vrai')
+    axs[2].plot(omega, label='Estimated')
+    axs[2].plot(omega_vrai, label='True Value')
     axs[2].set_ylabel('Omega')
     axs[2].set_title('Omega over Measures')
     axs[2].set_xlabel('Measure')
@@ -162,11 +138,13 @@ def plot_trace():
     for i in range(301):
         trace.append(np.trace(covariances[i]))
     ax.plot(trace)
-    ax.set_title("Trace by iteration")
+    ax.set_title("Evolution of the trace of the covariance matrix")
     ax.set_xlabel("Iteration")
-    ax.set_ylabel("Trace")
+    ax.set_ylabel("Trace of Covariance Matrix")
     ax.grid(alpha=0.3)
 
+plot_estimates()
 plot_trace()
+plot_error()
 plt.tight_layout()
 plt.show()
